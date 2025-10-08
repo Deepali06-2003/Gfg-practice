4d3df05ed90210a0fcf2be9e935d94d19ab16b5e
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-10-08 23:34:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -1,26 +1,28 @@
 class Solution {
   public:
-    int perfectSum(vector<int>& arr, int target) {
-        // code here
-        int n = arr.size();
-        
-       vector<vector<int>> dp (n+1 , vector<int>(target+1 , 0));
-       
-       for(int i =0;i<=n;i++){
-           dp[i][0]= 1;
-       }
-       
-       for(int i=1 ;i<=n;i++){
-           for(int j = 0;j<= target ;j++){
-               
-               if(arr[i-1] > j){
-                   dp[i][j]= dp[i-1][j];
-               }
-               else{
-                   dp[i][j] = dp[i-1][j] + dp[i-1][j- arr[i-1]];
-               }
-           }
-       }
-       return dp[n][target];
+    const int MOD = 1e9 + 7;
+
+int helper(vector<int>& arr, vector<vector<int>>& dp, int target, int i) {
+    if (i == 0) {
+        if (target == 0 && arr[0] == 0) return 2;
+        if (target == 0 || arr[0] == target) return 1;
+        return 0;
     }
+
+    if (dp[i][target] != -1) return dp[i][target];
+
+    int nt = helper(arr, dp, target, i - 1);
+    int t = 0;
+    if (arr[i] <= target) {
+        t = helper(arr, dp, target - arr[i], i - 1);
+    }
+
+    return dp[i][target] = (nt + t) % MOD;
+}
+
+int perfectSum(vector<int>& arr, int target) {
+    int n = arr.size();
+    vector<vector<int>> dp(n, vector<int>(target + 1, -1));
+    return helper(arr, dp, target, n - 1);
+}
 };
