1972a8df04ca3a272152ca929d3f53514b9d9e07
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-07-21 15:18:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,38 +1,51 @@
 class Solution {
   public:
-  bool dfs(vector<vector<int>>& adj , vector<int>& visited , int x, int parent){
+  
+  bool IsLoop(vector<vector<int>>& adjList, vector<int>& visited, int node){
+      visited[node]=1;
+                //node, parent
+      queue<pair<int, int>>q;
       
-      visited[x] =1;
-      for(auto j : adj[x]){
-         
-          if(!visited[j]){
-              if( dfs(adj, visited, j, x))return true;
+      q.push({node, -1});
+      
+      while(!q.empty()){
+          int temp = q.front().first;
+          int parent = q.front().second;
+          
+          q.pop();
+          
+          for(auto j : adjList[temp]){
+              if(visited[j]==0){
+                  visited[j]=1;
+                  q.push({j, temp});
+              }
+              else if(visited[j]==1 && j != parent) return true;
           }
-          else if(j != parent)return true;
       }
+      
       return false;
   }
-  
-  
     bool isCycle(int V, vector<vector<int>>& edges) {
         // Code here
-        vector<vector<int>>adj(V);
         
-        for(auto& p : edges){
-            adj[p[1]].push_back(p[0]);
-            adj[p[0]].push_back(p[1]);
+        vector<vector<int>> adjList(V);
+        
+        for(int i=0; i<edges.size(); i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            
+            adjList[u].push_back(v);
+            adjList[v].push_back(u);
         }
         
         vector<int>visited(V, 0);
-        for(int i =0;i<V;i++){
-            if(!visited[i]){
-                if(dfs(adj , visited , i, -1))return true;
+        
+        for(int i=0;i<V;i++){
+            if(visited[i]==0){
+                if(IsLoop(adjList, visited , i)) return true;;
             }
         }
         return false;
+        
     }
 };
-
-
-
-
