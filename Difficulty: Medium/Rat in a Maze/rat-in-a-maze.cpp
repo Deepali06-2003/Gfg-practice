6e5e343923ef1de935fed3638eedc545de3641ed
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-07-30 11:17:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rat in a Maze/rat-in-a-maze.cpp
+++ Difficulty: Medium/Rat in a Maze/rat-in-a-maze.cpp
@@ -1,29 +1,28 @@
 class Solution {
   public:
   
-  void helper(vector<vector<int>>& maze, int n, vector<string>& res, string curr, int i, int j){
-      if(i<0 || j<0 || i>=n || j>=n || maze[i][j]==0) return;
+  void helper(vector<vector<int>>& maze, int n , int m , vector<string>& res, string curr, int i, int j){
+      if(i<0 || j<0 || i>=n || j>=m || maze[i][j]==0) return;
       
-      if(i == n-1 && j == n-1){
+      if(i == n-1 && j==m-1){
           res.push_back(curr);
           return;
       }
-      maze[i][j] = 0;
-      helper(maze, n, res, curr+'D', i+1, j);
-      helper(maze, n, res, curr+'L', i, j-1);
-      helper(maze, n, res, curr+'R', i, j+1);
-      helper(maze, n, res, curr+'U', i-1, j);
-      maze[i][j] =1;
+      maze[i][j]=0;
+      helper(maze, n , m , res, curr+'D' , i+1, j);
+      helper(maze, n , m , res, curr+'L' , i, j-1);
+      helper(maze, n , m , res, curr+'R' , i, j+1);
+      helper(maze, n , m , res, curr+'U' , i-1, j);
+      maze[i][j]=1;
   }
     vector<string> ratInMaze(vector<vector<int>>& maze) {
+        // code here
         
-        int n = maze.size();
+        int n = maze.size() , m = maze[0].size();
         vector<string>res;
         string curr;
         
-        if(maze[0][0]==0 || maze[n-1][n-1]==0) return res;
-        
-        helper(maze, n , res, curr , 0, 0);
+        helper(maze, n , m , res, curr, 0, 0);
         return res;
     }
 };
