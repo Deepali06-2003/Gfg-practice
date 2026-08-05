e165a378d442e5d1476c4f913759c68e7b8f0e7c
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-08-05 09:34:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Graph Adjacency List Traversal/graph-adjacency-list-traversal.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    vector<vector<int>> printGraph(int V, vector<pair<int, int>>& edges) {
+        // code here
+        vector<vector<int>>adj(V);
+        for(int i=0;i<edges.size();i++){
+            adj[edges[i].first].push_back(edges[i].second);
+            adj[edges[i].second].push_back(edges[i].first);
+        }
+        
+        return adj;
+    }
+};
