341dca7807a10fc6f4b953756f012dd0e710b102
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-08-28 20:51:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,51 +1,36 @@
 class Solution {
   public:
   
-  bool IsLoop(vector<vector<int>>& adjList, vector<int>& visited, int node){
-      visited[node]=1;
-                //node, parent
-      queue<pair<int, int>>q;
+  bool helper(vector<vector<int>>& adj, vector<int>& visited, int n, int p){
       
-      q.push({node, -1});
+      visited[n]=1;
       
-      while(!q.empty()){
-          int temp = q.front().first;
-          int parent = q.front().second;
+      for(auto j: adj[n]){
           
-          q.pop();
-          
-          for(auto j : adjList[temp]){
-              if(visited[j]==0){
-                  visited[j]=1;
-                  q.push({j, temp});
-              }
-              else if(visited[j]==1 && j != parent) return true;
+          if(visited[j]== -1){
+              if(helper(adj, visited, j, n)) return true;
           }
+          else if(visited[j]==1 && j != p) return true;
       }
-      
       return false;
   }
     bool isCycle(int V, vector<vector<int>>& edges) {
         // Code here
         
-        vector<vector<int>> adjList(V);
-        
-        for(int i=0; i<edges.size(); i++){
-            int u = edges[i][0];
-            int v = edges[i][1];
-            
-            adjList[u].push_back(v);
-            adjList[v].push_back(u);
+        vector<vector<int>>adj(V);
+        for(int i=0;i<edges.size();i++){
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
         }
         
-        vector<int>visited(V, 0);
+        vector<int>visited(V, -1);
         
         for(int i=0;i<V;i++){
-            if(visited[i]==0){
-                if(IsLoop(adjList, visited , i)) return true;;
+            if(visited[i] == -1){
+                if(helper(adj, visited, i, -1) == true) return true;
             }
         }
-        return false;
         
+        return false;
     }
 };
