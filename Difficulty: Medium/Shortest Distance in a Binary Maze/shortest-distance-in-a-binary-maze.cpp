80e5ee8a832b960e18c981e3218939b205399312
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-08-28 23:42:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Distance in a Binary Maze/shortest-distance-in-a-binary-maze.cpp
+++ Difficulty: Medium/Shortest Distance in a Binary Maze/shortest-distance-in-a-binary-maze.cpp
@@ -6,37 +6,41 @@ class Solution {
         int n = mat.size();
         int m = mat[0].size();
         
-        if(mat[src[0]][src[1]] == 0 || mat[dest[0]][dest[1]] == 0)return -1;
+        if(mat[src[0]][src[1]]==0 || mat[dest[0]][dest[1]]==0) return -1;
+        if(src == dest) return 0;
+        
         queue<pair<int, int>>q;
+        vector<int>dx = {0, 0, 1, -1};
+        vector<int>dy = {1, -1, 0, 0};
         
         q.push({src[0], src[1]});
         mat[src[0]][src[1]] = 0;
         
-        vector<int>dx={0, 0, 1, -1};
-        vector<int>dy={ 1, -1, 0, 0};
-        
-        int ans = 0;
+        int ans =0;
         while(!q.empty()){
-            int sz = q.size();
             
+            int sz = q.size();
             for(int i=0;i<sz;i++){
-                auto temp = q.front();
+                
+                auto t = q.front();
                 q.pop();
                 
-                if(temp.first == dest[0] && temp.second== dest[1]) return ans;
                 for(int j=0;j<4;j++){
-                    int nx = temp.first + dx[j];
-                    int ny = temp.second + dy[j];
+                    int nx = t.first + dx[j];
+                    int ny = t.second + dy[j];
                     
                     if(nx>=0 && ny>=0 && nx<n && ny<m && mat[nx][ny]==1){
-                        q.push({nx, ny});
                         mat[nx][ny]=0;
+                        q.push({nx, ny});
+                        if((nx == dest[0]) && (ny==dest[1])) return ans+1;
                     }
                 }
             }
             ans++;
-            
         }
+        
         return -1;
     }
 };
+
+
