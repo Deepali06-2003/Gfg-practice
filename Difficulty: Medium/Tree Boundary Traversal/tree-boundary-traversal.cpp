7e4f97c91e4472dff8c367c7b9dd424a7247fb7e
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-09-08 20:20:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.cpp
+++ Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.cpp
@@ -1,53 +1,65 @@
+/* Node Structure
+class Node {
+  public:
+    int data;
+    Node* left, *right;
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+}; */
+
 class Solution {
   public:
-    bool isLeaf(Node* root)
-    {
-        return root->left == NULL && root->right == NULL;
-    }
-    
-    void leafNode(Node* root , vector<int>& res){
-        if(root== NULL){
-            return;
-        }
-        if(isLeaf(root)){
+  
+  void left(Node *root, vector<int>& res){
+       if(root == NULL) return;
+       
+       if (root->left != NULL || root->right != NULL)
             res.push_back(root->data);
-            return;
-        }
-        leafNode(root->left , res);
-        leafNode(root->right , res);
-    }
-    
-    void leftView(Node* root, vector<int>&res){
-        if(root== NULL || isLeaf(root)){
-            return;
-        }
-        res.push_back(root->data);
-        if(root->left)
-            leftView(root->left , res);
-        else
-            leftView(root->right , res);
-    }
-    
-    void rightView(Node* root , vector<int>& res){
-        if(root== NULL || isLeaf(root)){
-            return;
-        }
-        if(root->right)
-            rightView(root->right, res);
-        else
-            rightView(root->left, res);
-        res.push_back(root->data);  // add after recursion for reverse order
-    }
-    
-    vector<int> boundaryTraversal(Node *root) {
-        vector<int>res;
-        if(!root) return res;
-        if(!isLeaf(root)) res.push_back(root->data);
-        
-        leftView(root->left , res);     // ✅ corrected from root->right to root->left
-        leafNode(root , res);           // all leaves
-        rightView(root->right , res);   // right boundary in reverse
 
+        if (root->left)
+           left(root->left, res);
+       else
+            left(root->right, res);
+  }
+  void bottom(Node *root, vector<int>& res){
+      if(root == NULL) return;
+      
+      bottom(root->left, res);
+      if(root->left==NULL && root->right==NULL) res.push_back(root->data);
+      bottom(root->right, res);
+  }
+  
+  void right(Node *root, vector<int>& curr){
+       if(root == NULL) return;
+       
+       if(root->left != NULL || root->right != NULL) curr.push_back(root->data);
+       
+       if(root->right)
+        right(root->right, curr);
+       else
+        right(root->left, curr);
+  }
+  
+    vector<int> boundaryTraversal(Node *root) {
+        // code here
+        //bottom 
+        // left
+        //right
+        
+        vector<int>res;
+        res.push_back(root->data);
+        if(root->left == NULL && root->right ==NULL) return res;
+        
+        left(root->left, res);
+        bottom(root, res);
+        
+        vector<int>curr;
+        right(root->right, curr);
+        reverse(curr.begin(), curr.end());
+        
+        for(int i=0;i<curr.size();i++) res.push_back(curr[i]);
         return res;
     }
 };
