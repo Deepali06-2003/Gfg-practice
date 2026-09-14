974b1d82a6da1dc4d9c881ea844d702d66f34469
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-09-14 09:47:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -2,39 +2,40 @@ class Solution {
   public:
     vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
         // Code here
-        
-        vector<vector<pair<int, int>>> adj(V);
+        vector<vector<pair<int,int>>>adj(V);
         for(int i=0;i<edges.size();i++){
-            int u = edges[i][0];
-            int v = edges[i][1];
-            adj[u].push_back({v, edges[i][2]});
-            adj[v].push_back({u, edges[i][2]});
+            int u = edges[i][0] , v = edges[i][1], w = edges[i][2];
+            
+            adj[u].push_back({v, w});
+            adj[v].push_back({u, w});
         }
         
         vector<int>dist(V, INT_MAX);
-       priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        dist[src]=0;
+        priority_queue<pair<int, int> , vector<pair<int, int>> , greater<pair<int, int>>> pq;
+        
         pq.push({0, src});
+        dist[src]=0;
         
         while(!pq.empty()){
-            int d = pq.top().first;
-            int node = pq.top().second;
+            auto[wt, n] = pq.top();
             pq.pop();
             
-            if (d > dist[node]) continue;
-            for(auto j : adj[node]){
-                int n = j.first;
-                int c_d = j.second;
-                
-                if(dist[n] > c_d+d){
-                    dist[n] = c_d +d;
-                    pq.push({dist[n] , n});
+            if(wt > dist[n]) continue;
+            
+            for(auto j: adj[n]){
+                int w = j.second;
+                                                   int c = j.first;
+                if(dist[c] > dist[n]+w){
+                    dist[c] = dist[n]+w;
+                    pq.push({dist[c] , c});
                 }
             }
         }
-        for(int i=0;i<V;i++){
-            if(dist[i]==INT_MAX)dist[i] =-1;
+        
+        for(int& i :dist){
+            if(i==INT_MAX) i = -1;
         }
         return dist;
+        
     }
 };
